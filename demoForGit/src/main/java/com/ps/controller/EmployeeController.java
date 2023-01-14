c5ca3ac3413668cc4ed94a7f8f0b72eb110370f3
RajDadas <raj.dadas@gmail.com>
2023-01-14 23:18:44 +0530

    jj

--- demoForGit/src/main/java/com/ps/controller/EmployeeController.java
+++ demoForGit/src/main/java/com/ps/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.ps.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class EmployeeController {
+
+    @GetMapping
+    public  String get(){
+        return "jekk";
+    }
 }
